75040ea6eadd73010960614ca3c6c82bbadc2dc7
Andrew Phillips <andrewp@apache.org>
2014-10-10 14:53:28 -0400

    Catching up with provider removal in jclouds core
    * bluelock-vcloud-zone01 (7e04f62)
    * greenhousedata-element-vcloud (0acc61b)
    * JCLOUDS-735: ninefold-storage (1d95d29) and ninefold-compute (1d95d29)
    
    Also adding cloudstack as a feature explicitly since it's no longer
    transitively included via ninefold-compute

--- itests/src/test/java/org/jclouds/karaf/itests/MiscFeaturesInstallationTest.java
+++ itests/src/test/java/org/jclouds/karaf/itests/MiscFeaturesInstallationTest.java
@@ -50,11 +50,6 @@ public class MiscFeaturesInstallationTest extends JcloudsFeaturesTestSupport {
     }
 
     @Test
-    public void testBlueLockFeature() throws Exception {
-        installAndCheckFeature("jclouds-bluelock-vcloud-zone01");
-    }
-
-    @Test
     public void testGoGridFeature() throws Exception {
         installAndCheckFeature("jclouds-gogrid");
     }
@@ -62,17 +57,6 @@ public class MiscFeaturesInstallationTest extends JcloudsFeaturesTestSupport {
     @Test
     public void testGo2CloudFeature() throws Exception {
         installAndCheckFeature("jclouds-go2cloud-jhb1");
-    }
-
-
-    @Test
-    public void testGreenHouseDataFeature() throws Exception {
-        installAndCheckFeature("jclouds-greenhousedata-element-vcloud");
-    }
-
-    @Test
-    public void testNineFoldStorageFeature() throws Exception {
-        installAndCheckFeature("jclouds-ninefold-storage");
     }
 
     @Test
